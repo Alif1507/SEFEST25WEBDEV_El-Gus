98fc1230cce710203a8daa4d30d454cf78a8f5d9
Alif1507 <m.alifwahyudi2007@gmail.com>
2025-01-28 22:13:43 +0700

    edisi spesial

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,25 +1,100 @@
 import Navbar1 from "./Navbar1";
-import logo from "/logo.png";
+import goals from "/goals.png";
+import logo1 from "/LOGO2.png";
 import Button from "./components/Button";
+import hati from "/hati.png";
 import { Link } from "react-router";
 
 function Home() {
   return (
     <>
       <Navbar1 />
-      <body className="overflow-hidden">
-      <div className="cross h-screen flex w-screen items-center justify-around">
+      <div className="cross h-screen flex w-screen items-center justify-center gap-28">
         <div className="text-hero">
-          <h1 className="text-6xl font-bold abu p-2 rounded-br-2xl rounded-tr-2xl">Healthy Together </h1>
-          <p className="abu p-2 rounded-br-2xl rounded-tr-2xl">Ensure healthy lives and promote well-being for all at all ages <br />
-          Towards Health and Well-Being for All!</p>
-          <Link to="/mental"><button className="abu p-2 rounded-br-2xl rounded-tr-2xl cursor-pointer mt-3"><Button /></button></Link>
+          <h1 className="text-6xl font-bold abu p-2 rounded-br-2xl rounded-tr-2xl">
+            Healthy Together{" "}
+          </h1>
+          <p className="abu p-2 rounded-br-2xl rounded-tr-2xl">
+            Ensure healthy lives and promote well-being for all at all ages{" "}
+            <br />
+            Towards Health and Well-Being for All!
+          </p>
+          <Link to="/mental">
+            <button className="abu p-2 rounded-br-2xl rounded-tr-2xl cursor-pointer mt-3">
+              <Button />
+            </button>
+          </Link>
         </div>
-        <div className="w-96">
-          <img className="object-fill" src={logo} alt="" />
+        <div className="w-[600px] max-h-auto">
+          <img className="object-fill" src={logo1} alt="" />
         </div>
       </div>
-      </body>
+
+      <div className="flex flex-col justify-center items-center">
+        <div className="w-screen flex justify-center">
+          <h1 className="text-hero text-4xl font-bold drop-shadow-2xl mb-16">
+            APA ITU HEALTHY TOGETHER
+          </h1>
+        </div>
+        <div className="flex ijo w-5xl mx-24 rounded-3xl items-center">
+          <div className="w-[2500px] h-auto">
+            <img className="object-fill w-full max-h-auto" src={logo1} alt="" />
+          </div>
+          <div>
+            <p className="text-end text-lg  text-white mx-16">
+              Healthy Together adalah merupakan website yang memberikan
+              informasi tentang kesehatan, Web ini memberikan informasi seputar
+              kesehatan baik itu dari dampak, penyebab, akibat, ataupun solusi,
+              kami mengambil tema kesehatan yang dimana mengikuti salah satu
+              tema 17 SDGs yang dikelola United Nations yaitu no 3 Good Health
+              And Well-Being
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex flex-row justify-around mt-32  items-center">
+        <div className="flex flex-col">
+          <div className="text-hero">
+            <h1 className="text-3xl font-bold text-center mb-16">17SDGs</h1>
+          </div>
+          <div className="h-[423px] w-[444px] rounded-2xl shadow-2xl flex flex-col menjaga-border justify-around items-center">
+            <div>
+              <img src={goals} alt="" />
+            </div>
+            <p className="text-center mx-10 text-hero">
+              17 SDGs (Sustainable Development Goals) atau Tujuan Pembangunan
+              Berkelanjutan adalah agenda global yang terdiri dari 17 tujuan
+              utama yang diadopsi oleh Perserikatan Bangsa-Bangsa (PBB) pada
+              tahun 2015. Tujuan ini dirancang untuk menjadi panduan bagi
+              negara-negara di dunia dalam mencapai pembangunan yang
+              berkelanjutan secara sosial, ekonomi, dan lingkungan hingga tahun
+              2030.
+            </p>
+          </div>
+        </div>
+        <div className="flex flex-col">
+          <div className="text-hero">
+            <h1 className="text-3xl font-bold mb-16 text-center">
+              GOOD HEALTH AND WELL-BEING
+            </h1>
+          </div>
+          <div className="h-[423px] w-[444px] rounded-2xl shadow-2xl flex flex-col menjaga-border justify-around items-center">
+            <div>
+              <img src={hati} alt="" />
+            </div>
+            <p className="text-center mx-10 text-hero">
+              Good Health and Well-Being atau Kesehatan yang Baik dan
+              Kesejahteraan adalah tujuan ketiga dari 17 Sustainable Development
+              Goals (SDGs) yang bertujuan untuk memastikan kehidupan yang sehat
+              dan mempromosikan kesejahteraan untuk semua orang di semua umur.
+              Tujuan ini berfokus pada peningkatan akses terhadap layanan
+              kesehatan, pengurangan penyakit menular dan tidak menular, serta
+              mendorong gaya hidup sehat.
+            </p>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
--- src/Menjaga.jsx
+++ src/Menjaga.jsx
@@ -1,4 +1,22 @@
+import { useState } from "react";
+import satu from "/1.png";
+import dua from "/2.png";
+import tiga from "/3.png";
+import empat from "/4.png";
+import lima from "/5.png";
+import enam from "/6.png";
+import tujuh from "/7.png";
+import delapan from "/8.png";
+import sembilan from "/9.png";
+import { FaChevronDown, FaChevronRight } from "react-icons/fa";
+
 function Menjaga() {
+  const [buka, setBuka] = useState(false);
+
+  function clickHandler() {
+    setBuka(!buka);
+  }
+
   return (
     <>
       <div className="mt-32">
@@ -8,60 +26,159 @@ function Menjaga() {
           </h1>
         </div>
 
-        <div>
-          <div>
+        <div className="flex flex-col items-center gap-32">
+          <div className="flex">
+            <div className="w-[312px] h-[239px] menjaga-border flex flex-col-reverse justify-center items-center py-20 rounded-2xl">
+              <div className="mt-10">
+                <FaChevronDown
+                  className="cursor-pointer"
+                  onClick={clickHandler}
+                  color="#119586"
+                  size={30}
+                />
+              </div>
+              <h1 className="text-hero text-md font-bold">Terapi</h1>
+              <div className="h-96 w-auto">
+                <img className="object-fill" src={satu} alt="" />
+              </div>
+            </div>
+            
             <div>
-              <img src="" alt="" />
+              <div className="w-[312px] h-[239px] menjaga-border flex flex-col justify-center items-center py-20 rounded-2xl mx-32">
+                <img className="object-fill" src={dua} alt="" />
+                <h1 className="text-hero text-md font-bold">Meditasi</h1>
+                <div className="mt-10">
+                <FaChevronDown
+                  className="cursor-pointer"
+                  onClick={clickHandler}
+                  color="#119586"
+                  size={30}
+                />
+              </div>
+              </div>
+            </div>
+            <div className="w-[312px] h-[239px] menjaga-border flex flex-col justify-center items-center py-20 rounded-2xl">
+              <div className="h-96 w-auto">
+                <img className="object-fill" src={tiga} alt="" />
+                <h1 className="text-hero text-md font-bold text-center items-center">
+                  Olahraga
+                </h1>
+                <div className="ml-10 mt-10">
+                <FaChevronDown
+                  className="cursor-pointer"
+                  onClick={clickHandler}
+                  color="#119586"
+                  size={30}
+                />
+              </div>
+              </div>
             </div>
           </div>
-          <div>
-            <div>
-              <img src="" alt="" />
-            </div>
-          </div>
-          <div>
-            <div>
-              <img src="" alt="" />
-            </div>
-          </div>
-        </div>
 
-        <div>
-          <div>
+          <div className="flex">
+            <div className="w-[312px] h-[239px] menjaga-border flex flex-col-reverse justify-center items-center py-20 rounded-2xl">
+            <div className="mt-10">
+                <FaChevronDown
+                  className="cursor-pointer"
+                  onClick={clickHandler}
+                  color="#119586"
+                  size={30}
+                />
+              </div>
+              <h1 className="text-hero text-md font-bold">Pola Makan</h1>
+              <div className="h-96 w-auto">
+                <img className="object-fill" src={empat} alt="" />
+              </div>
+            </div>
             <div>
-              <img src="" alt="" />
+              <div className="w-[312px] h-[239px] menjaga-border flex flex-col justify-center items-center py-20 rounded-2xl mx-32">
+                <img className="object-fill" src={lima} alt="" />
+                <h1 className="text-hero text-md font-bold text-center items-center">
+                  MENGHINDARI KEBIASAAN BURUK
+                </h1>
+                <div className="mt-10">
+                <FaChevronDown
+                  className="cursor-pointer"
+                  onClick={clickHandler}
+                  color="#119586"
+                  size={30}
+                />
+              </div>
+              </div>
+            </div>
+            <div className="w-[312px] h-[239px] menjaga-border flex flex-col justify-center items-center gap-5 py-20 rounded-2xl">
+              <div className="h-72 w-auto mt-5">
+                <img className="object-cover" src={enam} alt="" />
+              </div>
+              <div>
+                <h1 className="text-hero text-md font-bold text-center items-center">
+                  MEMPERKUAT DUKUNGAN SOSIAL
+                </h1>
+                <div className="ml-30 mt-5">
+                <FaChevronDown
+                  className="cursor-pointer"
+                  onClick={clickHandler}
+                  color="#119586"
+                  size={30}
+                />
+              </div>
+              </div>
             </div>
           </div>
-          <div>
-            <div>
-              <img src="" alt="" />
-            </div>
-          </div>
-          <div>
-            <div>
-              <img src="" alt="" />
-            </div>
-          </div>
-        </div>
 
-        <div>
-          <div>
-            <div>
-              <img src="" alt="" />
+          <div className="flex">
+            <div className="w-[312px] h-[239px] menjaga-border flex flex-col-reverse justify-center items-center gapy-20 py-20 rounded-2xl">
+            <div className="mt-10">
+                <FaChevronDown
+                  className="cursor-pointer"
+                  onClick={clickHandler}
+                  color="#119586"
+                  size={30}
+                />
+              </div>
+              <h1 className="text-hero text-md font-bold text-center items-center">
+                MENJAGA KESEHATAN TUBUH
+              </h1>
+              <div className="h-96 w-auto">
+                <img className="object-fill" src={tujuh} alt="" />
+              </div>
             </div>
-          </div>
-          <div>
             <div>
-              <img src="" alt="" />
+              <div className="w-[312px] h-[239px] menjaga-border flex flex-col justify-center items-center gapy-20 py-20 rounded-2xl mx-32">
+                <img className="object-fill" src={delapan} alt="" />
+                <h1 className="text-hero text-md font-bold text-center items-center">
+                  TIDUR YANG CUKUP
+                </h1>
+                <div className="mt-10">
+                <FaChevronDown
+                  className="cursor-pointer"
+                  onClick={clickHandler}
+                  color="#119586"
+                  size={30}
+                />
+              </div>
+              </div>
             </div>
-          </div>
-          <div>
-            <div>
-              <img src="" alt="" />
+            <div className="w-[312px] h-[239px] menjaga-border flex flex-col justify-center items-center gap-5 py-20 rounded-2xl">
+              <div className="h-96 w-auto ">
+                <img className="object-fill" src={sembilan} alt="" />
+              </div>
+              <div>
+                <h1 className="text-hero text-md font-bold text-center items-center">
+                  MENGEMBANGKAN KETERAMPILAN KOPING{" "}
+                </h1>
+                <div className="ml-35 mt-4">
+                <FaChevronDown
+                  className="cursor-pointer"
+                  onClick={clickHandler}
+                  color="#119586"
+                  size={30}
+                />
+              </div>
+              </div>
             </div>
           </div>
         </div>
-        
       </div>
     </>
   );
